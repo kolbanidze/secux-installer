0ec1d62d56ae2cb27cc0a3e4814f6411d5c46853
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2025-02-21 12:37:42 +0300

    0.2.3

--- collect_python_packages.sh
+++ collect_python_packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pip download customtkinter face_recognition face_recognition_models -d python_packages
+pip download customtkinter face_recognition face_recognition_models screeninfo -d python_packages
