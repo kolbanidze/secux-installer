9df0804555afb788ebf2cd18a98f62c82743d479
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2025-02-23 21:49:06 +0300

    build changes

--- collect_python_packages.sh
+++ collect_python_packages.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-pip download customtkinter face_recognition face_recognition_models -d python_packages
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <output_dir>"
+    exit 1
+fi
+
+output_dir="$1"
+
+mkdir -p "$output_dir"
+
+pip download customtkinter face_recognition face_recognition_models -d "$output_dir"
