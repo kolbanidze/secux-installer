87d66f6cc2375ee38c556d27ea1593f885db3dc7
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2026-01-06 09:38:04 +0300

    why

--- secux-installer/main.py
+++ secux-installer/main.py
@@ -431,7 +431,7 @@ class InstallPage(Adw.NavigationPage):
                 f"sources={sources}\n"+\
                 "xkb-options=['grp:alt_shift_toggle']"
 
-                self.execute(['arch-chroot', mount_point, 'bash', '-c', f'echo -e "{gnome_override}" >> {mount_point}/usr/share/glib-2.0/schemas/90_keyboard.gschema.override'])
+                self.execute(['arch-chroot', mount_point, 'bash', '-c', f'echo -e "{gnome_override}" > {mount_point}/usr/share/glib-2.0/schemas/90_keyboard.gschema.override'])
 
                 self.execute(['arch-chroot', mount_point, 'glib-compile-schemas', '/usr/share/glib-2.0/schemas'])
 
